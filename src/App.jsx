a9d44349feda2ba4e34c0d4636212c86a2841164
SolForte <mateusbrandao1234@gmail.com>
2023-03-16 21:16:00 -0300

    Feat: Routes (empty)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cadastro from "./components/cadastro";
+import Habitos from "./components/habitos";
+import Historico from "./components/historico";
+import Hoje from "./components/hoje";
+import Login from "./components/login";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/cadastro" element={<Cadastro/>}/>
+        <Route path="/habitos" element={<Habitos/>}/>
+        <Route path="/hoje" element={<Hoje/>}/>
+        <Route path="/historico" element={<Historico/>}/>
+      </Routes>
+    </BrowserRouter>  
   );
 }
 
