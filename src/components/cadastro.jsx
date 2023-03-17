650b522434133f90ce77435bd8b788ece4a602d0
SolForte <mateusbrandao1234@gmail.com>
2023-03-16 22:04:37 -0300

    Login sem estilização

--- src/components/cadastro.jsx
+++ src/components/cadastro.jsx
@@ -1,3 +1,5 @@
 export default function Cadastro(){
-    
+    return(
+        <></>
+    )
 }
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,44 @@
 import styled from "styled-components";
 import logo from "../assets/logo.png";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function Login(){
+
+    const [formEmail, setFormEmail] = useState();
+    const [formPassword, setFormPassword] = useState();
+
+    function performLogin (){
+        return ("")
+    }
+
     return(
         <Main>
             <img src={logo} alt="TrackIt Logo"/>
+            <FormContainer>
+                <form onSubmit={performLogin}>
+                    <input
+                        placeholder="email"
+                        type="email"
+                        value={formEmail}
+                        onChange={(event) => setFormEmail(event.target.value)}
+                        required
+                    />
+                    <input
+                        placeholder="senha"
+                        type="password"
+                        value={formPassword}
+                        onChange={(event) => setFormPassword(event.target.value)}
+                        required
+                    />
+                    <button data-test="book-seat-btn" type="submit">
+                        Entrar
+                    </button>
+                </form>
+            </FormContainer>
+            <Link to="/cadastro">
+                Não tem uma conta? Cadastre-se
+            </Link>
         </Main> 
     )
 }
@@ -16,4 +51,10 @@ const Main = styled.div`
     img{
         width: 180px;
     }
+    a{
+        text-decoration: none;
+    }
 `
+
+const FormContainer = styled.div``
+
